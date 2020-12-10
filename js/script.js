7d0ad32cdf70ef3343e99bdd0247d4baf3c26033
Entity-Code <mattia.tummolilllo@gmail.com>
2020-12-11 00:23:09 +0100

    Sostituzione dati footer statici con dati dinamici da vue.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,63 @@
 var app = new Vue({
    el: "#app",
    data: {
-      hello: "hello"
+      // FOOTER
+      footer:{
+         // top
+         rightArrow: "fas fa-chevron-right",
+         //Landrick 1
+         title1: "img/logo-light.png", //logo
+         p1: "Start working with Landrick that can provide everything you need to generate awareness, drive traffic, connect.",
+         socials:[
+            "fab fa-facebook-f",
+            "fab fa-instagram",
+            "fab fa-twitter",
+            "fab fa-linkedin-in"
+         ],
+
+         // Company 2
+         title2: "Company",
+         links2: [
+            "About us",
+            "Services",
+            "Team",
+            "Pricing",
+            "Project",
+            "Careers",
+            "Blog",
+            "Login"
+         ],
+         // Usefull Links
+         title3: "Usefull Links",
+         links3: [
+            "Terms of Services",
+            "Privacy Policy",
+            "Documentation",
+            "Changelog",
+            "Components",
+         ],
+         // Newsletter 4
+         title4: "Newsletter",
+         p4: "Sign up and receive the latest tips via email.",
+         hdue4: ["Write your email", "*"],
+         emailDue4: ["far fa-envelope", "Your email:"],
+
+         // bottom
+         // left
+         credits: "© 2020 Landrick. Design with ❤️ by Shreethemes.",
+         //right
+         cards: [
+            "img/american-ex.png",
+            "img/discover.png",
+            "img/master-card.png",
+            "img/paypal.png",
+            "img/visa.png"
+         ]
+
+      }
+
+
+
    }
    // mounted: function() {
    //
